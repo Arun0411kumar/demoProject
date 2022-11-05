56d765f79fc10b8dc6687d603e2c9c20400eee07
arun <arunkumar.selvam@ideas2it.com>
2022-11-05 16:46:48 +0530

    second commit

---
+++ TwoWheeler.java
@@ -0,0 +1,39 @@
+public class TwoWheeler extends Vehicle {
+    protected byte noOfStroke;
+    protected String twoWheelerType;
+
+    public TwoWheeler(
+        String brandName, 
+        String fuelType, 
+        byte mileage, 
+        String colour, 
+        byte noOfStroke, 
+        String twoWheelerType,
+        String vehicleId
+    )
+    {
+	this.brandName = brandName;
+	this.fuelType = fuelType;
+	this.mileage = mileage;  
+	this.colour = colour;
+	this.noOfStroke = noOfStroke;
+	this.twoWheelerType = twoWheelerType; 
+        this.vehicleId = vehicleId;   
+    }
+
+    public byte getNoOfStroke() {
+        return noOfStroke;
+    }
+
+    public void setNoOfStroke(byte noOfStroke) {
+	this.noOfStroke = noOfStroke;
+    }
+
+    public String getTwoWheelerType() {
+        return twoWheelerType;
+    }
+
+    public void setTwoWheelerType(String twoWheelerType) {
+	this.twoWheelerType = twoWheelerType;
+    }
+}
