3b5a5cff37d2dce2e8266f139113a1cfb468fc3d
arun <arunkumar.selvam@ideas2it.com>
2022-11-05 12:15:51 +0530

    my first project

---
+++ FourWheeler.java
@@ -0,0 +1,29 @@
+public class FourWheeler extends Vehicle {
+    protected byte seatCapacity;
+
+    public FourWheeler(
+        String brandName, 
+        String fuelType, 
+        byte mileage, 
+        String colour, 
+        byte seatCapacity,
+        String vehicleId 
+    )
+    {
+	this.brandName = brandName;
+	this.fuelType = fuelType;
+	this.mileage = mileage;
+	this.colour = colour;
+	this.seatCapacity = seatCapacity;
+	this.vehicleId = vehicleId;
+    }
+
+    public byte getSeatCapacity() {
+        return seatCapacity;
+    }
+
+    public void setSeatCapacity(byte noOfStroke) {
+	this.seatCapacity = seatCapacity;
+    }
+
+}
